07dfcace92bba3cec520c8ead7e2e1c5dfafd313
Steve Aldworth <s.aldworth@tvdsb.ca>
2020-11-10 12:34:47 -0500

    Finished basic sound demo

--- Game1.cs
+++ Game1.cs
@@ -12,8 +12,16 @@ namespace Sounds_in_Monogame
 
         MouseState mouseState;
 
-        // Sound Effects
+        string arnoldStatus;
 
+        // Textures
+        Texture2D arnoldTexture;
+        Rectangle arnoldRect;
+
+        // Fonts
+        SpriteFont messageFont;
+
+        // Sound Effects
         SoundEffect backgroundMusic;
 
         SoundEffect terminatorSound;
@@ -30,8 +38,9 @@ namespace Sounds_in_Monogame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            arnoldStatus = "silent.";
             base.Initialize();
+            backgroundMusic.Play();
         }
 
         protected override void LoadContent()
@@ -39,10 +48,16 @@ namespace Sounds_in_Monogame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            arnoldTexture = Content.Load<Texture2D>("terminator");
+            arnoldRect = new Rectangle(_graphics.PreferredBackBufferWidth / 2 - 100, _graphics.PreferredBackBufferHeight / 2 - 50, 200, 100);
+
             terminatorSound = Content.Load<SoundEffect>("T1 Be Back");
             terminatorSoundInstance = terminatorSound.CreateInstance();
+            terminatorSoundInstance.IsLooped = false;   // The sound will only play once
 
             backgroundMusic = Content.Load<SoundEffect>("tng_intro_theme");
+
+            messageFont = Content.Load<SpriteFont>("TextFont");
         }
 
         protected override void Update(GameTime gameTime)
@@ -52,7 +67,18 @@ namespace Sounds_in_Monogame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // Plays "I'll be back" when the user clicks on Arnold.
+            if (mouseState.LeftButton == ButtonState.Pressed && arnoldRect.Contains(mouseState.X, mouseState.Y))
+            {
+                arnoldStatus = "talking.";
+                terminatorSoundInstance.Play();
+            }
+
+            // When the sound is done playing, updates Arnold's status
+            if(terminatorSoundInstance.State == SoundState.Stopped)
+            {
+                arnoldStatus = "silent.";
+            }
 
             base.Update(gameTime);
         }
@@ -62,6 +88,13 @@ namespace Sounds_in_Monogame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.DrawString(messageFont, "Arnold is " + arnoldStatus, new Vector2(320, 75), Color.White);
+            _spriteBatch.Draw(arnoldTexture, arnoldRect, Color.White);
+
+            _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
